7d2cf6e0fda7f3818ede480914a4643133beaa24
yangyanbo <yangyanbo@fuzhi.ai>
2024-03-19 17:54:46 +0800

    faet: wxss打包编译

--- src/commanders/build.js
+++ src/commanders/build.js
@@ -4,6 +4,7 @@ const { compileJson } = require('../compile/json');
 const { getModuleDeps } = require('../toolkit/getModuleDeps');
 const { compileWxml } = require('../compile/wxml');
 const { compileJS } = require('../compile/js');
+const { compileWxss } = require('../compile/wxss');
 
 function build(publicPath) {
   // 保存编译相关信息
@@ -16,9 +17,10 @@ function build(publicPath) {
   const moduleDeps = getModuleDeps();
   // wxml编译
   compileWxml(moduleDeps);
-
   // js编译
   compileJS();
+  // wxss编译
+  compileWxss(moduleDeps);
 }
 
 module.exports = {
---
+++ src/compile/wxss/index.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const postcss = require('postcss');
+const autoprefixer = require('autoprefixer');
+const { getWorkPath, getTargetPath } = require('../../env');
+
+async function compileWxss(moduleDeps) {
+  let cssMerge = await getCompileCssCode({
+    path: 'app',
+    moduleId: '',
+  });
+  const distPath = getTargetPath();
+  for (const path in moduleDeps) {
+    cssMerge += await getCompileCssCode({
+      path,
+      moduleId: moduleDeps[path].moduleId,
+    });
+  }
+  fs.writeFileSync(`${distPath}/app.wxss`, cssMerge, 'utf-8');
+}
+
+async function getCompileCssCode(opts) {
+  const { path, moduleId } = opts;
+  const workPath = getWorkPath();
+  const wxssFileFullPath = `${workPath}/${path}.wxss`;
+  const wxssCode = fs.readFileSync(wxssFileFullPath, 'utf-8');
+
+  // rpx => rem
+  const ast = postcss.parse(wxssCode);
+  ast.walk((node) => {
+    if (node.type === 'rule') {
+      node.walkDecls((decl) => {
+        decl.value = decl.value.replace(/rpx$/g, 'rem');
+      });
+    }
+  });
+  const tranUnitCode = ast.toResult().css;
+  // autoprefixer
+  const result = await transCode(tranUnitCode, moduleId);
+  return result;
+}
+
+function transCode(cssCode, moduleId) {
+  return new Promise((resolve) => {
+    postcss([addScopeId({ moduleId }), autoprefixer({ overrideBrowserslist: ['cover 99.5%'] })])
+      .process(cssCode, { from: undefined })
+      .then((result) => {
+        resolve(result.css + '\n');
+      });
+  });
+}
+
+function addScopeId(opts) {
+  const { moduleId } = opts;
+
+  function func() {
+    return {
+      postcssPlugin: 'addScopeId',
+      prepare() {
+        return {
+          OnceExit(root) {
+            root.walkRules((rule) => {
+              if (!moduleId) {
+                return;
+              }
+              rule.selector += `[data-v-${moduleId}]`;
+            });
+          },
+        };
+      },
+    };
+  }
+  func.postcss = true;
+  return func;
+}
+
+module.exports = {
+  compileWxss,
+};
